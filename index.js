54c16dad17225d974c7ed53dfddc6d019d69be57
David <dbravo7@yahoo.com>
2019-06-20 20:56:46 -0400

    Debugged search results appearing when invalid name inputted

--- index.js
+++ index.js
@@ -1,104 +1,121 @@
 'use strict';
 
-$(document).ready(function () {
-
+function watchForm() {
   $('form').submit(event => {
-    event.preventDefault(); 
+    event.preventDefault();
     // the const states is an array of state codes 
+    // the states variable is an array of state codes 
     const states = stateCodes(event);
-    validStateCodes(states);
-
     const max_num = parseInt($('#js-max-results').val());
-    
+
+  if (validStateCodes(states)) {
     getParkInfo(states, max_num);
-  });
-
-  function stateCodes(event) {
-    let codes = []; 
-    
-    codes.push($('#js-state-1').val().toUpperCase());
-    codes.push($('#js-state-2').val().toUpperCase());
-    codes.push($('#js-state-3').val().toUpperCase());
-    const states = codes.filter(ele => ele !== "" );
-    return states; 
-  }
-
-  function validStateCodes(states) {
-    const stateAbbreviations = [
-      'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA',
-      'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA',
-      'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
-      'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
-      'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'
-    ];
-
-    states.forEach(function(state) {
-      if (!(stateAbbreviations.includes(state))) {
-        alert('Invalid state name. Please enter abbreviated form of state name');
-      }
-    }); 
-  }
-
-  function getParkInfo(states, max_num) {
-    const key = "&api_key=3s2cp4J69269b5elC49u4e5cG3xkZMJwTiA5mGJp";
-    const queryString = createParamsString(states, max_num);
-    const url = "https://developer.nps.gov/api/v1/parks?" + queryString + key;
-
-    console.log(url);
-
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error(response.statusText);
-      })
-      .then(responseJson => displayResults(responseJson))
-      .catch(error => {
-        $('section').addClass('hidden');
-        $('#js-error-message').text(`Something went wrong: ${error.message}`);
-      });
-  }
-
-  function createParamsString(states, max_num) {
-    let string_collection = [];
-
-    for (let i = 0; i < states.length; i++) {
-      string_collection.push(`stateCode=${states[i]}`)
-    }
-    string_collection.push(`limit=${max_num}`);
-    return string_collection.join('&');
-  }
-
-  function displayResults(response) {
-    console.log(response);
-    removeResults();
-
-    for (let i = 0; i < response.data.length; i++) {
-
-      $('.results').append(
-        `<li><h3>${response.data[i].fullName}</h3>
-          <p>${response.data[i].description}</p> 
-          <a href=${response.data[i].url}>Go to website</a>`    
-      )}
-    $('section').removeClass('hidden');   
-  }
-
-  function removeResults() {
-    $('.results').empty();
   }
 });
+}
 
-//get function
-  // Add key to HTTP header
-  // compose url
-  // fetch with url and header
-  // call display results
+function getParkInfo(states, max_num) {
+  const key = "&api_key=3s2cp4J69269b5elC49u4e5cG3xkZMJwTiA5mGJp";
+  const queryString = createParamsString(states, max_num);
+  const url = "https://developer.nps.gov/api/v1/parks?" + queryString + key;
+
+  console.log(url);
+
+fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => displayResults(responseJson))
+  .catch(error => {
+    $('section').addClass('hidden');
+    $('#js-error-message').text(`Something went wrong: ${error.message}`);
+  });
+}
+
+function stateCodes(event) {
+  let codes = [];
+
+  codes.push($('#js-state-1').val().toUpperCase());
+  codes.push($('#js-state-2').val().toUpperCase());
+  codes.push($('#js-state-3').val().toUpperCase());
+  const states = codes.filter(ele => ele !== "");
+  return states;
+}
+
+function validStateCodes(states) {
+  const stateAbbreviations = [
+    'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA',
+    'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA',
+    'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND',
+    'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+    'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'
+  ];
+
+  if (states === undefined || states.length === 0) {
+    alert('Please input a state name.');
+    return false; 
+  }
+
+  for (let i = 0; i < states.length; i++) {
+    if (!(stateAbbreviations.includes(states[i]))) {
+      alert('Invalid state name. Please enter abbreviated form of state name.');
+      return false; 
+    }
+  }
+  return true; 
+}
+
+function getParkInfo(states, max_num) {
+  const key = "&api_key=3s2cp4J69269b5elC49u4e5cG3xkZMJwTiA5mGJp";
+  const queryString = createParamsString(states, max_num);
+  const url = "https://developer.nps.gov/api/v1/parks?" + queryString + key;
 
 
-// The parks in the given state must be displayed on the page.Include at least:
-// Full name
-// Description
-// Website URL
-// The user must be able to make multiple searches and see 
-// only the results for the current search.
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => {
+      $('section').addClass('hidden');
+      $('#js-error-message').text(`Something went wrong: ${error.message}`);
+    });
+}
+
+function createParamsString(states, max_num) {
+  let string_collection = [];
+
+  for (let i = 0; i < states.length; i++) {
+    string_collection.push(`stateCode=${states[i]}`);
+  }
+  string_collection.push(`limit=${max_num}`);
+  return string_collection.join('&');
+}
+
+function displayResults(response) {
+  console.log(response);
+  removeResults();
+
+  for (let i = 0; i < response.data.length; i++) {
+
+    $('.results').append(
+      `<li><h3>${response.data[i].fullName}</h3>
+      <p>${response.data[i].description}</p> 
+      <a href=${response.data[i].url}>Go to website</a>`
+  )}
+    $('section').removeClass('hidden');
+  }
+
+function removeResults() {
+  $('.results').empty();
+}
+
+$(watchForm);
